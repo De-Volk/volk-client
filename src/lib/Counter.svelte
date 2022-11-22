8894b0e67d9f8217103f20ac27502c61bddb64bb
은승균 <dmstmdrbs98@gmail.com>
2022-11-23 02:20:34 +0900

    Init Project
    프로젝트 생성
    - 스벨트 + TS

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  let count: number = 0;
+  $:doubleCount = count*2;
+  const increment = () => {
+    count += 1;
+  }
+  const decrement = ()=>{
+    count -= 1;
+  }
+  
+</script>
+
+<p>count {count}</p>
+<p>doubled {doubleCount}</p>
+<div class='button-wrapper'>
+  <button on:click={increment}>
+    increment
+  </button>
+  <button on:click={decrement}>
+    decrement
+  </button>
+</div>
+
+
+
+<style>
+  .button-wrapper{
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    gap:1rem;
+    width:100%;
+
+  }
+  button{
+    background-color: chocolate;
+    color:white;
+  }
+</style>
