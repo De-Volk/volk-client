a7a9fa42551787d502443c851a2fb23296eb9ddb
max.cha <max.cha@kakaocorp.com>
2022-11-25 02:48:53 +0900

    chore: alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      $components: path.resolve('./src/components'),
+      $pages: path.resolve('./src/pages'),
+      $stores: path.resolve('./src/stores'),
+      $utils: path.resolve('./src/utils'),
+    },
+  },
   server: {
     port: 3000,
   },
