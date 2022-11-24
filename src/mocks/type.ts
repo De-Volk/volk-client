7424ed0119d08a484685fec86a1c996511a86c71
max.cha <max.cha@kakaocorp.com>
2022-11-24 16:48:56 +0900

    feat: 유저,투표 msw 추가

---
+++ src/mocks/type.ts
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import type {
+  DefaultBodyType,
+  PathParams,
+  ResponseComposition,
+  RestContext,
+  RestRequest,
+} from 'msw';
+
+export type Props = (
+  req: RestRequest<never, PathParams<string>>,
+  res: ResponseComposition<DefaultBodyType>,
+  ctx: RestContext
+) => any;
