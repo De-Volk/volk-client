cd6af53dcc369db539670c9039b40b38c17bc0c6
은승균 <dmstmdrbs98@gmail.com>
2022-11-28 02:37:11 +0900

    feat: add Layout component with Header
    헤더를 사용하기 위한 레이아웃 컴포넌트 추가
    - 헤더 좌,우측 slot 추가

---
+++ src/components/frame/Layout.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  export let header: string;
+</script>
+
+<main class="container">
+  <header class="header">
+    <div class="left-content">
+      <slot name="headerLeftContent" />
+    </div>
+    <h2>{header}</h2>
+    <div class="right-content">
+      <slot name="headerRightContent" />
+    </div>
+  </header>
+  <slot />
+</main>
+
+<style>
+  .container {
+    position: relative;
+    width: 100%;
+    height: calc(100% - 50px - 60px);
+    padding-top: 50px;
+  }
+  .header {
+    position: fixed;
+    top: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 50px;
+    background-color: #ffffff;
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
+  }
+  .header h2 {
+    font-size: 20px;
+    font-weight: 600;
+  }
+  .header .left-content,
+  .header .right-content {
+    position: absolute;
+    top: 0;
+    height: 100%;
+    width: auto;
+    display: flex;
+    align-items: center;
+  }
+  .header .left-content {
+    left: 0px;
+  }
+  .header .right-content {
+    right: 0px;
+  }
+</style>
